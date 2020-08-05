0b66aae1fca36b0b6e6e58394e48fe86c2c9668d
Amith Gowda <37700560+amith-gowda@users.noreply.github.com>
2020-08-06 02:41:36 +0530

    update css and challenge 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,26 @@
+//resoponsive nav
+
+$(function() {
+	menu = $('nav ul');
+
+  $('#openup').on('click', function(e) {
+    e.preventDefault(); menu.slideToggle();
+  });
+  
+  $(window).resize(function(){
+    var w = $(this).width(); if(w > 480 && menu.is(':hidden')) {
+      menu.removeAttr('style');
+    }
+  });
+  
+  $('nav li').on('click', function(e) {                
+    var w = $(window).width(); if(w < 480 ) {
+      menu.slideToggle(); 
+    }
+  });
+  $('.open-menu').height($(window).height());
+});
+
 //Challenge 1: Your age in days
 
 function ageInDays () {
