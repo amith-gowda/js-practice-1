a387e25cea99b09b52aba635db3203392c3c3ee0
Amith Gowda <37700560+amith-gowda@users.noreply.github.com>
2020-08-06 16:09:37 +0530

    finished challenge 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,29 @@
 //resoponsive nav
 
-$(function() {
-	menu = $('nav ul');
-
-  $('#openup').on('click', function(e) {
-    e.preventDefault(); menu.slideToggle();
+$(function () {
+    menu = $("nav ul");
+  
+    $("#openup").on("click", function (e) {
+      e.preventDefault();
+      menu.slideToggle();
+    });
+  
+    $(window).resize(function () {
+      var w = $(this).width();
+      if (w > 480 && menu.is(":hidden")) {
+        menu.removeAttr("style");
+      }
+    });
+  
+    $("nav li").on("click", function (e) {
+      var w = $(window).width();
+      if (w < 480) {
+        menu.slideToggle();
+      }
+    });
+    $(".open-menu").height($(window).height());
   });
   
-  $(window).resize(function(){
-    var w = $(this).width(); if(w > 480 && menu.is(':hidden')) {
-      menu.removeAttr('style');
-    }
-  });
-  
-  $('nav li').on('click', function(e) {                
-    var w = $(window).width(); if(w < 480 ) {
-      menu.slideToggle(); 
-    }
-  });
-  $('.open-menu').height($(window).height());
-});
 
 //Challenge 1: Your age in days
 
@@ -28,9 +32,19 @@ function ageInDays () {
         document.getElementById('ageInDays').remove();
     }
     var birthYear = prompt('What year were you born in?');
-    var days = (2020 - birthYear)*365;
     var h1 = document.createElement('h1');
-    var textAnswer = document.createTextNode('You are ' + days + ' days old') ;
+    if (birthYear >= 2021) {
+        var textAnswer = document.createTextNode(birthYear + '? Are you a time traveller, bro?');
+        console.log(birthYear + '? Are you a time traveller, bro?')
+    }
+    else if (birthYear < 1900) {
+        var days = (2020 - birthYear)*365;
+        var textAnswer = document.createTextNode('You are ' + days + ' days old. How are you alive, bro?');
+    }
+    else {
+        var days = (2020 - birthYear)*365;
+        var textAnswer = document.createTextNode('You are ' + days + ' days old');
+    }
     h1.setAttribute('id', 'ageInDays');
     h1.appendChild(textAnswer);
     document.getElementById('fbResult').appendChild(h1);
